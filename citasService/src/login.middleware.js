5c3dd2823e9d85e67ea343f9b06a0000eca9c739
Ivan Yared Lombana Lozano <ilombana@unal.edu.co>
2023-12-05 23:54:17 -0500

    Auth verification

--- citasService/src/login.middleware.js
+++ citasService/src/login.middleware.js
@@ -4,7 +4,6 @@ export const loginRequired = (req, res, next) => {
     let token = req.headers['authorization']
     if (!token) return res.status(401).json({message: 'An access token is required to proceed'});
     token = token.split(' ')[1];
-    console.log(token, process.env.JWT_SECRET);
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
             return res.status(403).json({ message: 'Forbidden - Invalid token' });
@@ -12,8 +11,8 @@ export const loginRequired = (req, res, next) => {
 
         console.log("User authenticated");
 
-        req.user = user;
-        console.log("User", user);
+        //req.user = user;
+        //console.log("User", user);
         next();
     });
 }
